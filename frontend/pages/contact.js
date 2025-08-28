db16a9f9bd988440274f69eaae38992e5e2d37ef
dipanshumittal466-web <dipanshumittal466@gmail.com>
2025-08-28 14:08:51 +0530

    Delete frontend/pages directory
    Signed-off-by: dipanshumittal466-web <dipanshumittal466@gmail.com>

--- frontend/pages/contact.js
+++
@@ -1,9 +0,0 @@
-import Header from './components/header'
-export default function Contact() {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-      <p>Call/WhatsApp: 7668420528</p>
-    </div>
-  )
-}
