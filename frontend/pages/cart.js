ac7c61aa746a3c31a739de2789e371a7bb5a7492
dipanshumittal466-web <dipanshumittal466@gmail.com>
2025-08-28 16:55:35 +0530

    Update cart.js
    Signed-off-by: dipanshumittal466-web <dipanshumittal466@gmail.com>

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,5 +1,5 @@
 import Header from '../components/Header'
-import products from '../data/products.json'
+import products from '../products.json'
 import Link from 'next/link'
 
 export default function Cart(){
