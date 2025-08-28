fce9a322b49d37477e5ff0ef45023885c6ea0aaa
dipanshumittal466-web <dipanshumittal466@gmail.com>
2025-08-28 13:28:46 +0530

    Update index.js
    Signed-off-by: dipanshumittal466-web <dipanshumittal466@gmail.com>

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,4 @@
-import Header from '../components/Header'
+import Header from '../../components/Header'
 import ProductCard from '../components/ProductCard'
 import products from '../data/products.json'
 
