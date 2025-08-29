003256832c6ef0ae32e40f18a0d0eec146b40adc
dipanshumittal466-web <dipanshumittal466@gmail.com>
2025-08-29 13:28:43 +0530

    Update config.js
    Signed-off-by: dipanshumittal466-web <dipanshumittal466@gmail.com>

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,14 +1,3 @@
 // config.js
 const API_URL = "https://mittalindustry-backend.onrender.com/api"; 
 export default API_URL;
-{
-  "queries": [
-    "inverter battery",
-    "ceiling fan",
-    "LED bulb",
-    "stabilizer",
-    "copper wire",
-    "switch socket"
-  ],
-  "pages": 2
-}
