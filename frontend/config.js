2a019ea2f2fc4665fc0d9784f5eb70e9cb0e3aca
dipanshumittal466-web <dipanshumittal466@gmail.com>
2025-08-29 14:26:00 +0530

    Update config.js
    Signed-off-by: dipanshumittal466-web <dipanshumittal466@gmail.com>

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,4 @@
-// /config.js
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://mittalindustry-backend.onrender.com";
-export default API_URL; // keep default too if you want the simple import
+
 {
   "queries": [
     "inverter battery",
