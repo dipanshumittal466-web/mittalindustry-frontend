9e808c28cb12705058cc9f1779dfa56156e9ce0d
dipanshumittal466-web <dipanshumittal466@gmail.com>
2025-08-29 14:27:37 +0530

    Update config.js
    Signed-off-by: dipanshumittal466-web <dipanshumittal466@gmail.com>

--- frontend/config.js
+++ frontend/config.js
@@ -1,12 +1,4 @@
-
-{
-  "queries": [
-    "inverter battery",
-    "ceiling fan",
-    "LED bulb",
-    "stabilizer",
-    "copper wire",
-    "switch socket"
-  ],
-  "pages": 2
-}
+// /config.js
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://mittalindustry-backend.onrender.com";
+export default API_URL; // keep default too if you want the simple import
